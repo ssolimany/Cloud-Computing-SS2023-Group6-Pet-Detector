afe19fca21ccee716fc766b46ed79abe77ce1c2a
Saman Solimany <58730895+ssolimany@users.noreply.github.com>
2023-06-18 20:57:58 +0200

    Add missing parameters to sensor-node script

--- sensor-node/script.py
+++ sensor-node/script.py
@@ -4,9 +4,10 @@ from pathlib import Path
 from datetime import datetime
 import requests
 import base64
+import time  # Neu hinzugefügt
 
 # Flask backend URL
-BACKEND_URL = 'http://127.0.0.1:5001'
+BACKEND_URL = 'http://192.168.178.66:5001'
 
 def draw_boxes(img, detections, class_names):
     for det in detections:
@@ -15,39 +16,39 @@ def draw_boxes(img, detections, class_names):
         cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
         cv2.putText(img, class_name, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
 
-# YOLOv5-Modell laden
+# Load YOLOv5 model
 model = torch.hub.load('ultralytics/yolov5', 'custom', path='/home/gruppe6/Desktop/best_aktuell.pt')
 model.conf = 0.25  # confidence threshold
 
-# Klassenindizes in lesbare Namen umwandeln
+# Translate class indices to readable names
 class_names = {0: "hamster", 1: "dog", 2: "cat"}
 
-# Kamera initialisieren
+# Initialize camera
 cap = cv2.VideoCapture(0)
 
 try:
     while True:
-        # Kamerabild erfassen
+        # Capture camera image
         ret, frame = cap.read()
         if not ret:
             break
 
-        # YOLOv5-Analyse
+        # YOLOv5 analysis
         results = model(frame)
 
-        # Erkennungen filtern
+        # Filter detections
         detections = []
         for det in results.pred[0]:
-            if int(det[5]) in (0, 1, 2):  # 0: Hamster, 1: Hunde, 2: Katzen
+            if int(det[5]) in (0, 1, 2):  # 0: Hamster, 1: Dogs, 2: Cats
                 detections.append(det)
 
-        # Wenn Erkennungen vorhanden sind, an Flask Backend senden
+        # If detections are present, send to Flask backend
         if len(detections) > 0:
             draw_boxes(frame, detections, class_names)
-            detected_classes = "-".join([class_names[int(det[5])] for det in detections])
-            confidences = [det[4] for det in detections]
+            detected_classes = [class_names[int(det[5])] for det in detections]
+            confidences = [round(det[4].item(), 2) for det in detections]  # Convert tensor to Python number and round to 2 decimals
             timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
-            img_path = Path(f"{detected_classes}-{timestamp}.jpg")
+            img_path = Path(f"{detected_classes[0]}-{timestamp}.jpg")
 
             cv2.imwrite(str(img_path), frame)
             
@@ -60,8 +61,9 @@ try:
 
             data = {
                 'image_data': base64_encoded_image,
-                'detection_amount': detections,
-                'detection_confidence_values': confidences
+                'detection_classes': detected_classes,
+                'detection_confidence_values': confidences,
+                'detection_amount': len(confidences)
             }
 
             response = requests.post(url=f"{BACKEND_URL}/upload/jpeg", json=data)
@@ -75,14 +77,16 @@ try:
             # Print the response from the server
             print(response.text)
 
-        else:
-            print("Keine Erkennungen im aktuellen Frame")
+            # Sleep for 5 seconds
+            time.sleep(5)
 
-        # Anzeige des Videostreams
+        else:
+            print("No detections in the current frame")
+
+        # Display the video stream
         cv2.imshow("Video", frame)
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
-
 
 finally:
     cap.release()
